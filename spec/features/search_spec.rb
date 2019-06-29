9a0166849fb2a354cfdecdc18971a986288db3e5
Vasyl Pedak <truvazia@gmail.com>
2019-06-29 21:36:40 +0100

    Clarify the feature test

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 feature 'Nerds search', type: :feature do
-  scenario 'user finds a nerd' do
+  scenario 'user finds a nerd by username and sees their language' do
     visit '/'
 
     fill_in 'user_name', with: 'vasiliy-pdk'
     click_on 'Search'
 
     expect(page).to have_text('Vasyl Pedak')
+    expect(page).to have_text(/javascript/)
   end
 end
