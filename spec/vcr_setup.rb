b38ca855769497c10bc5b35393747d6d8bd07d4f
Vasyl Pedak <truvazia@gmail.com>
2019-07-07 16:05:46 +0100

    Make tests independent from the API token

--- spec/vcr_setup.rb
+++ spec/vcr_setup.rb
@@ -4,4 +4,5 @@ VCR.configure do |c|
   c.cassette_library_dir = File.join(__dir__, 'fixtures', 'vcr_cassettes')
   c.hook_into :webmock
   c.configure_rspec_metadata!
+  c.filter_sensitive_data('<GITHUB_API_ACCESS_TOKEN>') { ENV['GITHUB_API_ACCESS_TOKEN'] }
 end
